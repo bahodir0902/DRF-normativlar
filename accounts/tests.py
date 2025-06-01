04be3c452f49a19c6213a13afde68cda0f3bd7f0
bahodir <vbahodir00@gmail.com>
2025-06-01 20:37:03 +0500

    3-1

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from accounts.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.utils import generate_random_username
 
-# Create your tests here.
+class AccountsAPITest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='test@test.com', username=generate_random_username(), password='12')
+
+    def get_jwt_token(self):
+        return str(RefreshToken.for_user(self.user).access_token)
+
+    def test_register(self):
+        data = {
+            "email": "test@new.com",
+            "username": generate_random_username(),
+            "password": "12",
+            "re_password": "12"
+        }
+        response = self.client.post('/accounts/register/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_jwt(self):
+        data = {
+            "email": "test@test.com",
+            "password": "12"
+        }
+        response = self.client.post('/accounts/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from accounts.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.utils import generate_random_username
+from courses.models import Category
 
-# Create your tests here.
+class CoursesAPITest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='test@test.com', username=generate_random_username(), password='12')
+        self.category = Category.objects.create(name="Test category")
+
+    def get_jwt_token(self):
+        return str(RefreshToken.for_user(self.user).access_token)
+
+    def test_courses_crud(self):
+        #GET
+        response = self.client.get('/courses/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        #POST jwt bilan:
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.get_jwt_token()}")
+        data = {
+            "title": "Test",
+            "description": "Test",
+            "category": self.category.pk
+        }
+        response = self.client.post('/courses/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        #PUT
+        course_id = response.data['id']
+        response = self.client.put(f"/courses/{course_id}/", {"title": "Updated title"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        #Delete
+        response = self.client.delete(f"/courses/{course_id}/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_validation_error(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.get_jwt_token()}")
+        response = self.client.post("/courses/", {}) # -> bo'sh POST method, xatolik berishi kerak
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
