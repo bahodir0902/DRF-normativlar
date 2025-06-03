0a921ada6c349face467fbbbef27f3103b0904dd
bahodir <vbahodir00@gmail.com>
2025-06-03 16:22:00 +0500

    fixed CD, asgi.py and Daphne

--- config/asgi.py
+++ config/asgi.py
@@ -1,20 +1,14 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+from django import setup
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from notifications.routing import websocket_urlpatterns
 from notifications.middleware import JWTAuthMiddlewareStack
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
