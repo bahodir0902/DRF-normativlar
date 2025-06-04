fc0fe433a898043976b3e21965c3f1fed6c23263
bahodir <vbahodir00@gmail.com>
2025-06-04 17:21:29 +0500

    delete postgres from docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-until PGPASSWORD="$DB_PASSWORD" psql \
-    -h "$DB_HOST" \
-    -U "$DB_USER" \
-    -d "$DB_NAME" \
-    -c "\q" 2>/dev/null; do
-      echo "Postgres is unavailable - sleeping"
-    sleep 2
-done
-
-echo "Postgres is up - proceeding with migrations."
-
 python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --noinput
